bc7eea60a0d77333ef7c3272e983d4d06d8069a1
Blowa <blowaxd693@gmail.com>
2018-04-28 19:46:11 +0200

    fix publish build time

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,5 @@ rm -r dist
 
 # release both windows & linux version
 cd src/NosSharp.World
-dotnet publish --self-contained -r win-x64 -c release -o ../../dist/World/windows
 dotnet publish --self-contained -r linux-x64 -c release -o ../../dist/World/linux
 cd ..
