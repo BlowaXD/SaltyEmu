9cba9faf294de6f56d6e979040d877ba8a83ca68
Blowa <blowaxd693@gmail.com>
2018-06-09 21:18:36 +0200

    fix docker & plugins

--- scripts/init-docker.cmd
+++ scripts/init-docker.cmd
@@ -1,8 +1,3 @@
-docker run --expose 5432:5432 --name database -d postgres \
-    -e POSTGRES_USER="admin" \
-    -e POSTGRES_PASSWORD="admin" \
-    -e POSTGRES_DB="nossharp" \
-docker run --expose 6379:6379 --name redis -d redis
-docker run --expose 80:8080 --name pgadmin -d dpage/pgadmin4 \
-    -e PGADMIN_DEFAULT_EMAIL="admin@email.com" \
-    -e PGADMIN_DEFAULT_PASSWORD="admin" \
+docker run -p 5432:5432 --name database -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=nossharp -d postgres
+docker run -p 6379:6379 --name redis -d redis
+docker run -p 80:8080 --name pgadmin -e PGADMIN_DEFAULT_EMAIL=admin@email.com -e PGADMIN_DEFAULT_PASSWORD=admin --link database:database -d dpage/pgadmin4
