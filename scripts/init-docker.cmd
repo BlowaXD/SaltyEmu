f53657cdfeed42f69c818bfe28009788da9f82da
Blowa <blowaxd693@gmail.com>
2018-06-09 22:38:56 +0200

    fix docker wrong port

--- scripts/init-docker.cmd
+++ scripts/init-docker.cmd
@@ -1,3 +1,3 @@
 docker run -p 5432:5432 --name database -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=nossharp -d postgres
 docker run -p 6379:6379 --name redis -d redis
-docker run -p 80:8080 --name pgadmin -e PGADMIN_DEFAULT_EMAIL=admin@email.com -e PGADMIN_DEFAULT_PASSWORD=admin --link database:database -d dpage/pgadmin4
+docker run -p 8080:80 --name pgadmin -e PGADMIN_DEFAULT_EMAIL=admin@email.com -e PGADMIN_DEFAULT_PASSWORD=admin --link database:database -d dpage/pgadmin4
