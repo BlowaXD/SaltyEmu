06ddbdc969072d0aa12db043c19224505f0dc59a
Blowa <blowaxd693@gmail.com>
2018-08-21 12:53:11 +0200

    start UIHelper

---
+++ src/ChickenAPI/Helpers/UIHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace ChickenAPI.Game.Helpers
+{
+    public class UIHelper
+    {
+        // GenerateMessage (string + enum MessageType)
+        // GenerateDialog (question + Accept Packet + Refuse Packet)
+        // GenerateChatMessage
+    }
+}
