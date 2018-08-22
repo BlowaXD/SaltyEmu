43b68d97e2b6b6050a21203beeebb8a43bd1be7d
Blowa <blowaxd693@gmail.com>
2018-08-23 01:01:35 +0200

    add quicklist migration

--- src/Plugins/NosSharp.DatabasePlugin/Migrations/20180822230124_fix-quicklist.Designer.cs
+++ src/Plugins/NosSharp.DatabasePlugin/Migrations/20180822230124_fix-quicklist.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NosSharp.DatabasePlugin.Context;
 
 namespace NosSharp.DatabasePlugin.Migrations
 {
     [DbContext(typeof(NosSharpContext))]
-    partial class NosSharpContextModelSnapshot : ModelSnapshot
+    [Migration("20180822230124_fix-quicklist")]
+    partial class fixquicklist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Plugins/NosSharp.DatabasePlugin/Migrations/20180822230124_fix-quicklist.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NosSharp.DatabasePlugin.Migrations
+{
+    public partial class fixquicklist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Morph",
+                table: "quicklist");
+
+            migrationBuilder.DropColumn(
+                name: "Pos",
+                table: "quicklist");
+
+            migrationBuilder.DropColumn(
+                name: "Q1",
+                table: "quicklist");
+
+            migrationBuilder.DropColumn(
+                name: "Elo",
+                table: "character");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "quicklist",
+                newName: "RelatedSlot");
+
+            migrationBuilder.RenameColumn(
+                name: "Slot",
+                table: "quicklist",
+                newName: "Position");
+
+            migrationBuilder.RenameColumn(
+                name: "Q2",
+                table: "quicklist",
+                newName: "EnumType");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsQ1",
+                table: "quicklist",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSkill",
+                table: "quicklist",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsQ1",
+                table: "quicklist");
+
+            migrationBuilder.DropColumn(
+                name: "IsSkill",
+                table: "quicklist");
+
+            migrationBuilder.RenameColumn(
+                name: "RelatedSlot",
+                table: "quicklist",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "Position",
+                table: "quicklist",
+                newName: "Slot");
+
+            migrationBuilder.RenameColumn(
+                name: "EnumType",
+                table: "quicklist",
+                newName: "Q2");
+
+            migrationBuilder.AddColumn<short>(
+                name: "Morph",
+                table: "quicklist",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "Pos",
+                table: "quicklist",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "Q1",
+                table: "quicklist",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Elo",
+                table: "character",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
