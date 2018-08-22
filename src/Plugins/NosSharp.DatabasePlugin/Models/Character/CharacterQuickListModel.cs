1924fe736bf0b25313308e43a8146bcb43530935
Blowa <blowaxd693@gmail.com>
2018-08-23 01:01:05 +0200

    update quicklist model

--- src/Plugins/NosSharp.DatabasePlugin/Models/Character/CharacterQuickListModel.cs
+++ src/Plugins/NosSharp.DatabasePlugin/Models/Character/CharacterQuickListModel.cs
@@ -18,17 +18,11 @@ namespace NosSharp.DatabasePlugin.Models.Character
         [ForeignKey("FK_CHARACTERQUICKLIST_TO_CHARACTER")]
         public long CharacterId { get; set; }
 
-        [Required]     
-        public short Morph { get; set; }
+        public bool IsSkill { get; set; }
+        public short RelatedSlot { get; set; }
+        public short Position { get; set; }
 
-        public short Pos { get; set; }
-
-        public short Q1 { get; set; }
-
-        public short Q2 { get; set; }
-
-        public short Slot { get; set; }
-
-        public short Type { get; set; }
+        public short EnumType { get; set; }
+        public bool IsQ1 { get; set; }
     }
 }
