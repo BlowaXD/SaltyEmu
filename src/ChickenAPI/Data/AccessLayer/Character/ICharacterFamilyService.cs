52bb75493fdcb6b722d94331e76972aeca267265
Blowa <blowaxd693@gmail.com>
2018-08-29 16:03:56 +0200

    add basic Services Interfaces to manage Family & CharacterFamilies

---
+++ src/ChickenAPI/Data/AccessLayer/Character/ICharacterFamilyService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ChickenAPI.Core.Data.AccessLayer;
+using ChickenAPI.Game.Data.TransferObjects.Character;
+
+namespace ChickenAPI.Game.Data.AccessLayer.Character
+{
+    public interface ICharacterFamilyService : IMappedRepository<CharacterFamilyDto>
+    {
+        CharacterFamilyDto GetCharacterFamilyByCharacterId(long characterId);
+        Task<CharacterFamilyDto> GetCharacterFamilyByCharacterIdAsync(long characterId);
+
+        IEnumerable<CharacterFamilyDto> GetCharacterFamilyDtosByFamilyId(long familyId);
+        Task<IEnumerable<CharacterFamilyDto>> GetCharacterFamilyDtosByFamilyIdAsync(long familyId);
+    }
+}
---
+++ src/ChickenAPI/Data/AccessLayer/Families/IFamilyService.cs
@@ -0,0 +1,15 @@
+﻿using ChickenAPI.Core.Data.AccessLayer;
+using ChickenAPI.Game.Data.TransferObjects.Families;
+
+namespace ChickenAPI.Game.Data.AccessLayer.Families
+{
+    public interface IFamilyService : IMappedRepository<FamilyDto>
+    {
+        /// <summary>
+        /// Broadcast an UpdateFamily event that will force sync on clients
+        /// Will use ISC
+        /// </summary>
+        /// <param name="familyId"></param>
+        void UpdateFamily(long familyId);
+    }
+}
