ea334a9a63930220caacbf7d822d3398909d1c5c
Syl Eze <syleze.contact@gmail.com>
2018-08-31 22:41:54 +0200

    Add preq packet

---
+++ src/ChickenAPI.Packets/Game/Client/_NotYetSorted/PreqPacket.cs
@@ -0,0 +1,9 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.Game.Client._NotYetSorted
+{
+    [PacketHeader("preq")]
+    public class PreqPacket : PacketBase
+    {
+    }
+}
