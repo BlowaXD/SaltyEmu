c9d7c397df4ca4b8cba695d4454535c625d51861
Blowa <blowaxd693@gmail.com>
2018-09-03 14:30:05 +0800

    add some more scripts for windows & remove SpecialCtrlHandler

--- src/World/WorldServer.cs
+++ src/World/WorldServer.cs
@@ -157,9 +157,6 @@ namespace World
         private static void Main()
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-#if DEBUG
-            SetConsoleCtrlHandler(ConsoleCtrlCheck, true);
-#endif
             AppDomain.CurrentDomain.UnhandledException += Exit;
             AppDomain.CurrentDomain.ProcessExit += Exit;
             Console.CancelKeyPress += Exit;
@@ -207,49 +204,5 @@ namespace World
             LogManager.Shutdown();
             Console.ReadLine();
         }
-
-
-        // Declare the SetConsoleCtrlHandler function
-        // as external and receiving a delegate.
-
-#if DEBUG
-
-
-        private static bool ConsoleCtrlCheck(CtrlTypes ctrlType)
-        {
-            // Put your own handler here
-
-            switch (ctrlType)
-            {
-                case CtrlTypes.CTRL_CLOSE_EVENT:
-                    Exit(null, null);
-                    break;
-                default:
-                    Exit(null, null);
-                    break;
-            }
-
-            return true;
-        }
-
-        [DllImport("Kernel32")]
-        public static extern bool SetConsoleCtrlHandler(HandlerRoutine handler, bool add);
-
-        // A delegate type to be used as the handler routine
-        // for SetConsoleCtrlHandler.
-        public delegate bool HandlerRoutine(CtrlTypes ctrlType);
-
-        // An enumerated type for the control messages
-        // sent to the handler routine.
-
-        public enum CtrlTypes
-        {
-            CTRL_C_EVENT = 0,
-            CTRL_BREAK_EVENT,
-            CTRL_CLOSE_EVENT,
-            CTRL_LOGOFF_EVENT = 5,
-            CTRL_SHUTDOWN_EVENT
-        }
-#endif
     }
 }
