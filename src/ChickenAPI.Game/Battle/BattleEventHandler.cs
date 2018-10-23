f5b222710dbd798c34f56986f47aba327c295caa
Blowa <blowaxd693@gmail.com>
2018-10-23 17:13:29 +0800

    add some more details

--- src/ChickenAPI.Game/Battle/BattleEventHandler.cs
+++ src/ChickenAPI.Game/Battle/BattleEventHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using ChickenAPI.Game.Battle.Events;
+using ChickenAPI.Game.Battle.Hitting;
 using ChickenAPI.Game.ECS.Entities;
 using ChickenAPI.Game.Events;
 
@@ -8,16 +9,37 @@ namespace ChickenAPI.Game.Battle
 {
     public class BattleEventHandler : EventHandlerBase
     {
-        public override ISet<Type> HandledTypes { get; }
+        public override ISet<Type> HandledTypes => new HashSet<Type>
+        {
+            typeof(ProcessHitRequestEvent),
+            typeof(FillHitRequestEvent)
+        };
+
         public override void Execute(IEntity entity, ChickenEventArgs e)
         {
             switch (e)
             {
                 case ProcessHitRequestEvent processHitRequest:
+                    // do the whole graphical & stats processing
                     break;
                 case FillHitRequestEvent fillHitRequest:
+                    SetupHitType(fillHitRequest.HitRequest);
+                    FillEffects(fillHitRequest.HitRequest);
+                    DamageCalculation(fillHitRequest.HitRequest);
                     break;
             }
+        }
+
+        private void DamageCalculation(HitRequest hitRequest)
+        {
+        }
+
+        private void FillEffects(HitRequest hitRequest)
+        {
+        }
+
+        private void SetupHitType(HitRequest hitRequest)
+        {
         }
     }
 }
