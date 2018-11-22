7b0d63005368101a91a3ce2a7298226d5093ea6a
Zagith <geogrimaldi@icloud.com>
2018-11-22 15:37:36 +0100

    [ADD] EqInfoPacket event

---
+++ src/Plugins/NosSharp.PacketHandler/Inventory/EqInfoPacketHandling.cs
@@ -0,0 +1,19 @@
+﻿using ChickenAPI.Game.Entities.Player;
+using ChickenAPI.Game.Inventory.Args;
+using ChickenAPI.Packets.Game.Server.Inventory;
+
+namespace NosSharp.PacketHandler.Inventory
+{
+    public class EqInfoPacketHandling
+    {
+        public static void OnEqInfoPacket(EquipmentInfoPacket packet, IPlayerEntity player)
+        {
+            player.EmitEvent(new InventoryEqInfoEventArgs
+            {
+                Type = packet.Type,
+                Slot = packet.Slot,
+                ShopOwnerId = packet.ShopOwnerId
+            });
+        }
+    }
+}
