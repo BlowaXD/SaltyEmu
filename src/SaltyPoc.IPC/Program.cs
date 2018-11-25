8734a19ac6b6cda8b38f40a697c705da1e2c88d4
Blowa <blowaxd693@gmail.com>
2018-11-25 17:43:00 +0800

    update MQTT PoC

--- src/SaltyPoc.IPC/Program.cs
+++ src/SaltyPoc.IPC/Program.cs
@@ -1,31 +1,18 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using ChickenAPI.Core.IPC;
 using ChickenAPI.Core.Logging;
-using ChickenAPI.Game.NpcDialog;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Extensions.ManagedClient;
-using SaltyEmu.Communication.Utils;
-using SaltyPoc.IPC.Packets;
 
 namespace SaltyPoc.IPC
 {
     internal class Program
     {
         private static readonly Logger Log = Logger.GetLogger<Program>();
-        private static IIpcClient _client;
 
-        internal static IIpcRequestHandler GetHandler()
-        {
-            IIpcRequestHandler requestHandler = new RequestHandler();
-            requestHandler.Register<TestRequestPacket>(async packet =>
-            {
-                await packet.ReplyAsync(new TestResponsePacket());
-                Log.Info("Replied to TestRequest");
-            });
-            return requestHandler;
-        }
 
         internal static async Task Main(string[] args)
         {
@@ -36,32 +23,40 @@ namespace SaltyPoc.IPC
                 .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
                 .WithClientOptions(new MqttClientOptionsBuilder()
                     .WithClientId("Client1")
-                    .WithTcpServer("broker.hivemq.com")
-                    .WithTls().Build())
+                    .WithTcpServer("localhost")
+                    .Build())
                 .Build();
 
             IManagedMqttClient mqttClient = new MqttFactory().CreateManagedMqttClient();
-            await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("my/topic").Build());
+            await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("families").Build());
             await mqttClient.StartAsync(options);
+
+
+            mqttClient.Connected += (sender, eventArgs) =>
+            {
+                Log.Info("Connected !");
+                Task.Run(() => { Send(mqttClient).Wait(); });
+            };
+            mqttClient.ApplicationMessageReceived += (sender, eventArgs) =>
+            {
+                Log.Info($"Message Received {eventArgs.ApplicationMessage.Topic} : {Encoding.UTF8.GetString(eventArgs.ApplicationMessage.Payload)}");
+            };
+            mqttClient.ConnectingFailed += (sender, eventArgs) => { Log.Error("[CONNECTION_FAILED]", eventArgs.Exception); };
+
 
             // StartAsync returns immediately, as it starts a new thread using Task.Run, 
             // and so the calling thread needs to wait.
             Console.ReadLine();
         }
 
-        private static async Task Test()
+        private static async Task Send(IManagedMqttClient mqttClient)
         {
-            var req = new TestRequestPacket();
-            Log.Info("RequestPacket : " + req.Id);
-            TestResponsePacket resp = await _client.RequestAsync<TestResponsePacket>(req);
-            if (resp == null)
-            {
-                return; // not handled correctly
-            }
-
-            Log.Info("ResponsePacket : " + resp.Id);
-            Log.Info("ResponsePacket : " + resp.RequestId);
-            Log.Info("ResponsePacket : " + resp.RandomPropertyAsATry);
+            DateTime tmp = DateTime.Now.AddMilliseconds(500);
+            await mqttClient.PublishAsync(new MqttApplicationMessageBuilder()
+                .WithTopic("families")
+                .WithPayload(Encoding.UTF8.GetBytes("mabite")).Build());
+            await Task.Delay(tmp - DateTime.Now);
+            await Send(mqttClient);
         }
     }
 }
