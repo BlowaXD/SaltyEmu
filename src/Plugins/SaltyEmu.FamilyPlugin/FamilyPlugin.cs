b1ed9480bf2987dfbd3885f84a2c6ecbba307b25
Blowa <blowaxd693@gmail.com>
2018-11-28 16:33:02 +0800

    begin FamilyPlugin

---
+++ src/Plugins/SaltyEmu.FamilyPlugin/FamilyPlugin.cs
@@ -0,0 +1,34 @@
+﻿using ChickenAPI.Core.Plugins;
+
+namespace SaltyEmu.FamilyPlugin
+{
+    public class FamilyPlugin : IPlugin
+    {
+        public PluginEnableTime EnableTime => PluginEnableTime.PreContainerBuild;
+        public string Name => nameof(FamilyPlugin);
+
+        public void OnDisable()
+        {
+        }
+
+        public void OnEnable()
+        {
+        }
+
+        public void OnLoad()
+        {
+        }
+
+        public void ReloadConfig()
+        {
+        }
+
+        public void SaveConfig()
+        {
+        }
+
+        public void SaveDefaultConfig()
+        {
+        }
+    }
+}
