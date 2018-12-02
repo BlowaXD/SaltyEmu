c8c21b5fa68d1f14ee436665f96b573deed93e12
Blowa <blowaxd693@gmail.com>
2018-12-03 00:40:18 +0800

    improve IGameConfiguration (floats + more configuration on Shop interaction)

--- src/ChickenAPI.Game/IGameConfiguration.cs
+++ src/ChickenAPI.Game/IGameConfiguration.cs
@@ -6,42 +6,42 @@
         /// Includes :
         /// Xp won on monsters
         /// </summary>
-        uint GlobalXpMultiplier { get; set; }
+        float GlobalXpMultiplier { get; set; }
 
         /// <summary>
         /// Includes
         /// Xp won on monsters
         /// </summary>
-        uint GlobalJobXpMultiplier { get; set; }
+        float GlobalJobXpMultiplier { get; set; }
 
         /// <summary>
         /// Includes
         /// Xp won on monsters
         /// </summary>
-        uint GlobalSpXpMultiplier { get;set; }
+        float GlobalSpXpMultiplier { get; set; }
 
         /// <summary>
         /// Includes xp won on Act 6+ Monsters
         /// </summary>
-        uint GlobalHeroXpMultiplier { get; set; }
+        float GlobalHeroXpMultiplier { get; set; }
 
         /// <summary>
         /// Includes
         /// Xp won on monsters (1 monster = 1 xp)
         /// </summary>
-        uint GlobalFairyXpMultiplier { get; set; }
+        float GlobalFairyXpMultiplier { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        uint GoldWinOnDropMultiplier { get; set; }
+        float GoldWinOnDropMultiplier { get; set; }
 
         /// <summary>
         /// Includes
         /// Xp won on monsters
         /// Xp won from Medals
         /// </summary>
-        uint GlobalReputationMultiplier { get; set; }
+        float GlobalReputationMultiplier { get; set; }
 
         /// <summary>
         /// The daily SP points you are supposed to have
@@ -52,5 +52,12 @@
         /// The Sp Points you can store with cards or Sp Points potions
         /// </summary>
         uint SpMaxAdditionalPoints { get; set; }
+
+        #region Gold
+
+        float ItemPriceOnSellMultiplier { get; set; }
+        float ItemPriceOnBuyMultiplier { get;set; }
+
+        #endregion Gold
     }
 }
--- src/Plugins/NosSharp.TemporaryMapPlugins/JsonGameConfiguration.cs
+++ src/Plugins/NosSharp.TemporaryMapPlugins/JsonGameConfiguration.cs
@@ -4,14 +4,16 @@ namespace SaltyEmu.BasicPlugin
 {
     public class JsonGameConfiguration : IGameConfiguration
     {
-        public uint GlobalXpMultiplier { get; set; } = 1;
-        public uint GlobalJobXpMultiplier { get; set; } = 1;
-        public uint GlobalSpXpMultiplier { get; set; } = 1;
-        public uint GlobalHeroXpMultiplier { get; set; } = 1;
-        public uint GlobalFairyXpMultiplier { get; set; } = 1;
-        public uint GoldWinOnDropMultiplier { get; set; } = 1;
-        public uint GlobalReputationMultiplier { get; set; } = 1;
+        public float GlobalXpMultiplier { get; set; } = 1.0f;
+        public float GlobalJobXpMultiplier { get; set; } = 1.0f;
+        public float GlobalSpXpMultiplier { get; set; } = 1.0f;
+        public float GlobalHeroXpMultiplier { get; set; } = 1.0f;
+        public float GlobalFairyXpMultiplier { get; set; } = 1.0f;
+        public float GoldWinOnDropMultiplier { get; set; } = 1.0f;
+        public float GlobalReputationMultiplier { get; set; } = 1.0f;
         public uint SpMaxDailyPoints { get; set; } = 10000;
         public uint SpMaxAdditionalPoints { get; set; } = 1000000;
+        public float ItemPriceOnSellMultiplier { get; set; } = 1.0f;
+        public float ItemPriceOnBuyMultiplier { get; set; } = 1.0f;
     }
 }
