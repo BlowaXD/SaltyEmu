97f4732e9eea4f0ccffcbf3ca2a56892204ffd5a
Blowa <blowaxd693@gmail.com>
2018-12-03 00:11:51 +0800

    start basic GameConfiguration (rates..)

---
+++ src/ChickenAPI.Game/IGameConfiguration.cs
@@ -0,0 +1,46 @@
+﻿namespace ChickenAPI.Game
+{
+    public interface IGameConfiguration
+    {
+        /// <summary>
+        /// Includes :
+        /// Xp won on monsters
+        /// </summary>
+        uint GlobalXpMultiplier { get; set; }
+
+        /// <summary>
+        /// Includes
+        /// Xp won on monsters
+        /// </summary>
+        uint GlobalJobXpMultiplier { get; set; }
+
+        /// <summary>
+        /// Includes
+        /// Xp won on monsters
+        /// </summary>
+        uint GlobalSpXpMultiplier { get;set; }
+
+        /// <summary>
+        /// Includes xp won on Act 6+ Monsters
+        /// </summary>
+        uint GlobalHeroXpMultiplier { get; set; }
+
+        /// <summary>
+        /// Includes
+        /// Xp won on monsters (1 monster = 1 xp)
+        /// </summary>
+        uint GlobalFairyXpMultiplier { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        uint GoldWinOnDropMultiplier { get; set; }
+
+        /// <summary>
+        /// Includes
+        /// Xp won on monsters
+        /// Xp won from Medals
+        /// </summary>
+        uint GlobalReputationMultiplier { get; set; }
+    }
+}
