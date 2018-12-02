4edf783f1a75560e3a2dbcc86195ce8ec9eb609f
Quarry <oktawianzak@gmail.com>
2018-12-02 18:46:56 +0100

    SMemoPacketType added

---
+++ src/ChickenAPI.Enums/Packets/SMemoPacketType.cs
@@ -0,0 +1,10 @@
+namespace ChickenAPI.Enums.Packets
+{
+    public enum SMemoPacketType
+    {
+        SuccesPlayer = 0,
+        SuccesNPC = 1,
+        FailNPC = 2,
+        FailPlayer = 3
+    }
+}
