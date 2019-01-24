5660d16482af8bca64a3fc2c5e7ceafc52b8f996
Blowa <blowaxd693@gmail.com>
2019-01-24 23:12:57 +0000

    fix redis repositories

--- src/libs/SaltyEmu.Redis/GenericRedisCacheClient.cs
+++ src/libs/SaltyEmu.Redis/GenericRedisCacheClient.cs
@@ -14,12 +14,31 @@ namespace SaltyEmu.Redis
     public abstract class GenericRedisCacheClient<TObject> : ISynchronizedRepository<TObject> where TObject : class, ISynchronizedDto
     {
         protected readonly Logger Log = Logger.GetLogger($"RedisCache({typeof(TObject).Name})");
-        protected readonly string Prefix;
+        protected readonly string DataPrefix;
+        protected readonly string KeySetKey;
         protected readonly RedisHybridCacheClient CacheClient;
+
+        protected Task<CacheValue<ICollection<string>>> KeySet => CacheClient.GetSetAsync<string>(KeySetKey);
+
+        protected async Task<ICollection<string>> GetAllKeysAsync()
+        {
+            return (await KeySet).Value;
+        }
+
+        protected Task RegisterKeyAsync(IEnumerable<Guid> keys)
+        {
+            return CacheClient.SetAddAsync(KeySetKey, keys.Select(ToKey));
+        }
+
+        protected Task RemoveKeyAsync(IEnumerable<Guid> keys)
+        {
+            return CacheClient.SetRemoveAsync(KeySetKey, keys.Select(ToKey));
+        }
+
 
         protected string ToKey(Guid id)
         {
-            return Prefix + ":" + id;
+            return DataPrefix + id;
         }
 
         protected string ToKey(TObject obj) => ToKey(obj.Id);
@@ -28,16 +47,16 @@ namespace SaltyEmu.Redis
         {
         }
 
-        private GenericRedisCacheClient(string prefix, RedisConfiguration conf)
+        private GenericRedisCacheClient(string basePrefix, RedisConfiguration conf)
         {
-            Prefix = prefix;
+            DataPrefix = basePrefix + ":data:";
+            KeySetKey = basePrefix + ":keys:set";
             var tmp = new RedisCacheClientOptions
             {
                 ConnectionMultiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions
                 {
                     Password = conf.Password,
                     EndPoints = { $"{conf.Host}:{conf.Port}" },
-                    ChannelPrefix = prefix,
                 }),
                 Serializer = new JsonNetSerializer(new JsonSerializerSettings
                 {
@@ -49,7 +68,7 @@ namespace SaltyEmu.Redis
 
         public IEnumerable<TObject> Get()
         {
-            return CacheClient.GetAllAsync<TObject>().ConfigureAwait(false).GetAwaiter().GetResult().Values.Select(s => s.Value);
+            return CacheClient.GetAllAsync<TObject>(GetAllKeysAsync().ConfigureAwait(false).GetAwaiter().GetResult()).ConfigureAwait(false).GetAwaiter().GetResult().Values.Select(s => s.Value);
         }
 
         public TObject GetById(Guid id)
@@ -64,22 +83,26 @@ namespace SaltyEmu.Redis
 
         public TObject Save(TObject obj)
         {
+            RegisterKeyAsync(new[] { obj.Id }).ConfigureAwait(false).GetAwaiter().GetResult();
             CacheClient.SetAsync(ToKey(obj), obj).ConfigureAwait(false).GetAwaiter().GetResult();
             return obj;
         }
 
         public void Save(IEnumerable<TObject> objs)
         {
+            RegisterKeyAsync(objs.Select(obj => obj.Id)).ConfigureAwait(false).GetAwaiter().GetResult();
             CacheClient.SetAllAsync(objs.ToDictionary(ToKey, o => o)).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
         public void DeleteById(Guid id)
         {
+            RemoveKeyAsync(new[] { id }).ConfigureAwait(false).GetAwaiter().GetResult();
             CacheClient.RemoveAsync(ToKey(id)).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
         public void DeleteByIds(IEnumerable<Guid> ids)
         {
+            RemoveKeyAsync(ids).ConfigureAwait(false).GetAwaiter().GetResult();
             CacheClient.RemoveAllAsync(ids.Select(ToKey)).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
@@ -100,22 +123,26 @@ namespace SaltyEmu.Redis
 
         public async Task<TObject> SaveAsync(TObject obj)
         {
+            await RegisterKeyAsync(new[] { obj.Id });
             await CacheClient.SetAsync(ToKey(obj), obj);
             return obj;
         }
 
         public async Task SaveAsync(IEnumerable<TObject> objs)
         {
+            await RegisterKeyAsync(objs.Select(s => s.Id));
             await CacheClient.SetAllAsync(objs.ToDictionary(ToKey, o => o));
         }
 
         public async Task DeleteByIdAsync(Guid id)
         {
+            await RemoveKeyAsync(new[] { id });
             await CacheClient.RemoveAsync(ToKey(id));
         }
 
         public async Task DeleteByIdsAsync(IEnumerable<Guid> ids)
         {
+            await RemoveKeyAsync(ids);
             await CacheClient.RemoveAllAsync(ids.Select(ToKey));
         }
     }
--- src/libs/SaltyEmu.Redis/MappedCacheClientBase.cs
+++ src/libs/SaltyEmu.Redis/MappedCacheClientBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ChickenAPI.Data;
@@ -11,12 +12,29 @@ namespace SaltyEmu.Redis
 {
     public class MappedCacheClientBase<TObject> : IMappedRepository<TObject> where TObject : class, IMappedDto
     {
-        protected readonly string Prefix;
+        protected readonly string DataPrefix;
+        protected readonly string KeySetKey;
         protected readonly ICacheClient CacheClient;
+        protected Task<CacheValue<ICollection<string>>> KeySet => CacheClient.GetSetAsync<string>(KeySetKey);
+
+        protected async Task<ICollection<string>> GetAllKeysAsync()
+        {
+            return (await KeySet).Value;
+        }
+
+        protected Task RegisterKeyAsync(IEnumerable<long> keys)
+        {
+            return CacheClient.SetAddAsync(KeySetKey, keys.Select(ToKey));
+        }
+
+        protected Task RemoveKeyAsync(IEnumerable<long> keys)
+        {
+            return CacheClient.SetRemoveAsync(KeySetKey, keys.Select(ToKey));
+        }
 
         protected string ToKey(long id)
         {
-            return Prefix + ":" + id;
+            return DataPrefix + id;
         }
 
         protected string ToKey(TObject obj) => ToKey(obj.Id);
@@ -25,9 +43,10 @@ namespace SaltyEmu.Redis
         {
         }
 
-        private MappedCacheClientBase(string prefix, RedisConfiguration conf)
+        private MappedCacheClientBase(string basePrefix, RedisConfiguration conf)
         {
-            Prefix = prefix;
+            DataPrefix = basePrefix + ":data:";
+            KeySetKey = basePrefix + ":keys:set";
             CacheClient = new RedisHybridCacheClient(new RedisCacheClientOptions
             {
                 ConnectionMultiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions
@@ -43,43 +62,47 @@ namespace SaltyEmu.Redis
 
         public IEnumerable<TObject> Get()
         {
-            return CacheClient.GetAllAsync<TObject>(null).Result.Select(s => s.Value.Value);
+            return CacheClient.GetAllAsync<TObject>(GetAllKeysAsync().ConfigureAwait(false).GetAwaiter().GetResult()).Result.Select(s => s.Value.Value);
         }
 
         public TObject GetById(long id)
         {
-            return CacheClient.GetAsync<TObject>(ToKey(id)).Result.Value;
+            return CacheClient.GetAsync<TObject>(ToKey(id)).ConfigureAwait(false).GetAwaiter().GetResult().Value;
         }
 
         public IEnumerable<TObject> GetByIds(IEnumerable<long> ids)
         {
-            return CacheClient.GetAllAsync<TObject>(ids.Select(ToKey)).Result.Select(s => s.Value.Value);
+            return CacheClient.GetAllAsync<TObject>(ids.Select(ToKey)).ConfigureAwait(false).GetAwaiter().GetResult().Select(s => s.Value.Value);
         }
 
         public TObject Save(TObject obj)
         {
-            CacheClient.SetAsync(ToKey(obj), obj).Wait();
+            RegisterKeyAsync(new[] { obj.Id }).ConfigureAwait(false).GetAwaiter().GetResult();
+            CacheClient.SetAsync(ToKey(obj), obj).ConfigureAwait(false).GetAwaiter().GetResult();
             return obj;
         }
 
         public void Save(IEnumerable<TObject> objs)
         {
-            CacheClient.SetAllAsync(objs.ToDictionary(ToKey, o => o)).Wait();
+            RegisterKeyAsync(objs.Select(s => s.Id)).ConfigureAwait(false).GetAwaiter().GetResult();
+            CacheClient.SetAllAsync(objs.ToDictionary(ToKey, o => o)).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
         public void DeleteById(long id)
         {
-            CacheClient.RemoveAsync(ToKey(id)).Wait();
+            RemoveKeyAsync(new[] { id }).ConfigureAwait(false).GetAwaiter().GetResult();
+            CacheClient.RemoveAsync(ToKey(id)).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
         public void DeleteByIds(IEnumerable<long> ids)
         {
-            CacheClient.RemoveAllAsync(ids.Select(ToKey));
+            RemoveKeyAsync(ids).ConfigureAwait(false).GetAwaiter().GetResult();
+            CacheClient.RemoveAllAsync(ids.Select(ToKey)).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
         public async Task<IEnumerable<TObject>> GetAsync()
         {
-            return (await CacheClient.GetAllAsync<TObject>(null)).Select(s => s.Value.Value);
+            return (await CacheClient.GetAllAsync<TObject>(await GetAllKeysAsync())).Select(s => s.Value.Value);
         }
 
         public async Task<TObject> GetByIdAsync(long id)
@@ -94,22 +117,26 @@ namespace SaltyEmu.Redis
 
         public async Task<TObject> SaveAsync(TObject obj)
         {
+            await RegisterKeyAsync(new[] { obj.Id });
             await CacheClient.SetAsync(ToKey(obj), obj);
             return obj;
         }
 
         public async Task SaveAsync(IEnumerable<TObject> objs)
         {
+            await RegisterKeyAsync(objs.Select(s => s.Id));
             await CacheClient.SetAllAsync(objs.ToDictionary(ToKey, o => o));
         }
 
         public async Task DeleteByIdAsync(long id)
         {
+            await RemoveKeyAsync(new[] { id });
             await CacheClient.RemoveAsync(ToKey(id));
         }
 
         public async Task DeleteByIdsAsync(IEnumerable<long> ids)
         {
+            await RemoveKeyAsync(ids);
             await CacheClient.RemoveAllAsync(ids.Select(ToKey));
         }
     }
