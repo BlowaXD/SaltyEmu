dcbca5908a47e602b1b053c28d61e63586e91318
Blowa <blowaxd693@gmail.com>
2019-01-27 09:38:22 +0800

    add database creation during initialization script

--- scripts/init_everything.cmd
+++ scripts/init_everything.cmd
@@ -8,11 +8,17 @@ call .\scripts\management\pull_dockers.bat
 echo "Starting database..."
 call .\scripts\management\start_bdd.bat
 
+echo "Creating saltyemu database..."
+call .\scripts\management\create_dev_bdd.bat
+
 echo "Starting session..."
 call .\scripts\management\start_session.bat
 
 echo "Starting MQTT Broker..."
 call .\scripts\management\start_mqtt_broker.bat
+
+:: echo "Creating plugins volume..." // todo
+:: call .\scripts\management\create_plugins_volume.bat
 
 echo "Building Login Docker..."
 call .\scripts\build\build_docker_Login.bat
